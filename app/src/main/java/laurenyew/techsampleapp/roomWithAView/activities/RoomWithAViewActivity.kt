0c94657d696ded32b085b3e06f54afe6146ab6a0
Lauren Yew <laurenyew@gmail.com>
2019-08-10 17:32:36 -0500

    Hook up View w/ View Model

--- app/src/main/java/laurenyew/techsampleapp/roomWithAView/activities/RoomWithAViewActivity.kt
+++ app/src/main/java/laurenyew/techsampleapp/roomWithAView/activities/RoomWithAViewActivity.kt
@@ -1,11 +1,19 @@
 package laurenyew.techsampleapp.roomWithAView.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.roomwithaview_activity.*
 import laurenyew.techsampleapp.R
+import laurenyew.techsampleapp.roomWithAView.data.Word
 import laurenyew.techsampleapp.roomWithAView.views.WordListAdapter
+import laurenyew.techsampleapp.viewmodels.WordViewModel
 
 /***
  * LiveData + Room + MVVM
@@ -13,6 +21,8 @@ import laurenyew.techsampleapp.roomWithAView.views.WordListAdapter
  * https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/#0
  **/
 class RoomWithAViewActivity : AppCompatActivity() {
+
+    private lateinit var wordViewModel: WordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +32,35 @@ class RoomWithAViewActivity : AppCompatActivity() {
         val adapter = WordListAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fab.setOnClickListener {
+            val intent = Intent(this@RoomWithAViewActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+
+        //Setup state
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, Observer { words ->
+            words?.let {
+                adapter.setWords(it)
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.let {
+                val word = Word(it.getStringExtra(NewWordActivity.EXTRA_REPLY))
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    companion object {
+        const val newWordActivityRequestCode = 1
     }
 }
